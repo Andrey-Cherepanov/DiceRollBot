ada7ca2edfe22b9cbee95e0d966a28733f71855a
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-07-12 20:11:55 +0500

    handle command /start

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -6,7 +6,12 @@ from lexicon.lexicon import LEXICON
 
 router: Router = Router()
 
-# Handler for command \help
+# Handler for command /start
+@router.message(Command(commands='start'))
+async def process_start_command(message):
+    await message.answer(LEXICON['/start'])
+
+# Handler for command /help
 @router.message(Command(commands='help'))
 async def process_help_command(message):
     await message.answer(LEXICON['/help'])
