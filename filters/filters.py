96783808c80a35376288ffd70560011863db7e4e
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-07-20 18:23:17 +0500

    rewright filter via re

--- filters/filters.py
+++ filters/filters.py
@@ -1,17 +1,9 @@
 from aiogram.filters import BaseFilter
-from string import digits
+import re
 
 class IsRoll(BaseFilter):
     def __init__(self):
         pass
 
     async def __call__(self, message):
-        text = message.text
-        allowed_chars = ''.join(digits) + 'dkK +-/*'
-        for c in text:
-            if c not in allowed_chars:
-                return False
-        if text.count('d') != 1:
-            return False
-        result = (text[0] in digits and 'd' in text) or (text[0] == 'd')
-        return result
+        return bool(re.fullmatch(r'/?\d*[dD]\d+[ +\-*/kK\d]*', message.text))
